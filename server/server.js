22dec3958903e04375212cd13f2493a25d8bab95
adityadixit07 <adityadixit9660@gmail.com>
2024-11-13 22:15:55 +0530

    port update

--- server/server.js
+++ server/server.js
@@ -22,6 +22,6 @@ io.on("connection", (socket) => {
 
 app.use("/api", require("./routes/matchRoutes"));
 const PORT = process.env.PORT || 3001;
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT} `);
 });
