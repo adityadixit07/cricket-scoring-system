be277de2d0f592a7398b965fe65a259d1b7477ed
adityadixit07 <adityadixit9660@gmail.com>
2024-11-13 22:56:27 +0530

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainBoard from "./components/MainBoard";
 
 const App = () => {
   return (
-    <div>
-      <MainBoard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainBoard />} />
+      </Routes>
+    </Router>
   );
 };
 
